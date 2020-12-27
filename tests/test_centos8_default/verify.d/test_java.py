605882317625bae51405328b2d0f1ac39015f865
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2020-12-27 02:08:05 +0100

    change tests from openjdk-12 to openjdk-15
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: Id42ff738766a5d21e904b68adfacc4286e3ca083
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- tests/test_centos8_default/verify.d/test_java.py
+++ tests/test_centos8_default/verify.d/test_java.py
@@ -1,2 +1,4 @@
 def test_java_exists(host):
-    assert host.file("/opt/openjdk-12/bin/java").exists
+    assert host.file("/opt/openjdk-15/bin/java").exists
+def test_java2_exists(host):
+    assert host.file("/opt/openjdk-15/bin/java2").exists
